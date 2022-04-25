1c10dd50397b9c69c4dd08843c1b97b36a27c5a7
Jameskillr <amiteshjames00718@gmail.com>
2022-04-25 22:16:57 +0530

    version 3.0

--- js/index.js
+++ js/index.js
@@ -8,29 +8,29 @@ function hideMenu() {
   navLinks.style.right = "-200px";
 }
 
-// var icon = document.getElementById("icon");
+var icon = document.getElementById("icon");
 
-// if (localStorage.getItem("mode") == null) {
-//   localStorage.setItem("mode", "light");
-// }
+if (localStorage.getItem("mode") == null) {
+  localStorage.setItem("mode", "light");
+}
 
-// let localData = localStorage.getItem("mode");
+let localData = localStorage.getItem("mode");
 
-// if (localData == "light") {
-//   icon.src = "./icons/moon.png";
-//   document.body.classList.remove("dark-mode");
-// } else if (localData == "dark") {
-//   icon.src = "./icons/sun.png";
-//   document.body.classList.add("dark-mode");
-// }
+if (localData == "light") {
+  icon.src = "./icons/moon.png";
+  document.body.classList.remove("dark-mode");
+} else if (localData == "dark") {
+  icon.src = "./icons/sun.png";
+  document.body.classList.add("dark-mode");
+}
 
-// icon.onclick = function () {
-//   document.body.classList.toggle("dark-mode");
-//   if (document.body.classList.contains("dark-mode")) {
-//     icon.src = "./icons/sun.png";
-//     localStorage.setItem("mode", "dark");
-//   } else {
-//     icon.src = "./icons/moon.png";
-//     localStorage.setItem("mode", "light");
-//   }
-// };
+icon.onclick = function () {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    icon.src = "./icons/sun.png";
+    localStorage.setItem("mode", "dark");
+  } else {
+    icon.src = "./icons/moon.png";
+    localStorage.setItem("mode", "light");
+  }
+};
