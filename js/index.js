3d88f415f434635fb3d5d5588d1984082c0e5d20
Jameskillr <amiteshjames00718@gmail.com>
2022-06-14 18:09:25 +0530

    3 comit

--- js/index.js
+++ js/index.js
@@ -34,3 +34,9 @@ icon.onclick = function () {
     localStorage.setItem("mode", "light");
   }
 };
+
+// scroll
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
